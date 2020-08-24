d241163283ef8013db563f068ebc38afb46b8ae8
swaraj-jain <happy2000jain@gmail.com>
2020-08-24 19:05:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var images = [
+
+    "https://pbs.twimg.com/media/EaUCSPLUMAEPiIf.jpg",
+    "https://i.ytimg.com/vi/aAE02ZN3T9g/maxresdefault.jpg",
+    "https://qph.fs.quoracdn.net/main-qimg-df1c7310a2024cd4e71c17ebc4cef19f.webp"
+
+  ]
+
+  var imageHead = document.getElementById("tc");
+  var i = 0;
+setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      imageHead.style.opacity="0.9";
+      imageHead.style.transitionDuration="1s"
+      imageHead.style.backgroundRepeat="no-repeat"
+      imageHead.style.position="relative";
+      imageHead.style.backgroundAttachment="fixed";
+      imageHead.style.minHeight="50vh"
+      imageHead.style.backgroundSize="cover";
+
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+}, 2000);
